e9c3f825c7b935ccdf0dcdf81300be9d94ef1c21
Miya <foenx0315@gmail.com>
2022-07-02 17:19:14 +0530

    Create en.js

---
+++ locales/en.js
@@ -0,0 +1,17 @@
+module.exports = {
+  /** Title Text */
+  site_name: 'TopicBotList',
+//nav bar
+  main_nav: {
+    about: 'About',
+    discord: 'Discord',
+    all_bots: 'Bots',
+    partners: 'Partners',
+  },
+  //user nav
+  user_nav: {
+    profile: 'Profile',
+    add_bot: 'Add Bot',
+    cert_bot: 'Certification',
+  },
+};
