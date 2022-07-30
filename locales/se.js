ddc5d395f0e6e07106ca2954761d6eae4d2dfbb6
Miya <foenx0315@gmail.com>
2022-07-30 21:58:57 +0530

    Create se.js

---
+++ locales/se.js
@@ -0,0 +1,18 @@
+module.exports = {
+    /** Title Text */
+    site_name: 'TopicList',
+    /** NavBar Text */
+    main_nav: {
+      about: 'Om oss',
+      discord: 'Disharmoni',
+      all_bots: 'Bots',
+      partners: 'Partners',
+    },
+    /** User Nav Text */
+    user_nav: {
+      profile: 'Profil',
+      panel: 'Panel',
+      add_bot: 'Lägg till bot',
+      cert_bot: 'Certifiering',
+    },
+  };
