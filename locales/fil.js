52cb56ddb77619339e441336487909135fa7ecf3
Hachiki <ninonakano408@gmail.com>
2022-08-01 23:22:14 +0800

    Create fil.js
    filipino

---
+++ locales/fil.js
@@ -0,0 +1,18 @@
+//hachiki - wilardzy
+module.exports = {
+  /** Title Text */
+  site_name: 'TopicBotList',
+//nav bar
+  main_nav: {
+    about: 'Tungkol',
+    discord: 'Discord',
+    bots: 'Bots',
+    partners: 'Kaanib',
+  },
+  //user nav
+  user_nav: {
+    profile: 'Itsura',
+    add_bot: 'Mag-lagay ng Bot',
+    certfi_bot: 'Sertipikasyon',
+  },
+};
