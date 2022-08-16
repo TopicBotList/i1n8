d28ed0852c6f1936c7f0a84d46e8a0982b66ac7f
Juki <jukidev@protonmail.com>
2022-08-16 18:57:41 +0200

    Create pl.js

---
+++ locales/pl.js
@@ -0,0 +1,17 @@
+module.exports = {
+    /** Title Text */
+    site_name: 'Topic Lista Botów',
+    //nav bar 
+    main_nav: {
+        about: 'O',
+        discord: 'Discord',
+        bots: 'Boty',
+        partners: 'Partnerzy',
+    },
+    //user nav 
+    user_nav: {
+        profile: 'Profil',
+        add_bot: 'Dodaj Bota',
+        certfi_bot: 'Certyfikacja',
+    },
+};
